6a503402a1d0faa69bf3b94ee2837cae52e70022
gerard <gerardvisablasco@gmail.com>
2021-01-17 19:44:06 +0100

    Added some logs with different levels

--- src/main/java/com/minigames/fizzbuzz/controller/GameController.java
+++ src/main/java/com/minigames/fizzbuzz/controller/GameController.java
@@ -2,6 +2,8 @@ package com.minigames.fizzbuzz.controller;
 
 import com.minigames.fizzbuzz.functional.GameProcessor;
 import com.minigames.fizzbuzz.representation.Game;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
+    Logger logger = LoggerFactory.getLogger(GameController.class);
+
     @Value("${fizzbuzz.targetNumber:4000}")
     private int targetNumber;
 
     @GetMapping("/game")
     public Game getFizzBuzzCompletedGame(@RequestParam(value = "starting_number") int startingNumber) {
+        logger.debug("GameController values: targetNumber = " + targetNumber + ", startingNumber = " + startingNumber);
+        long startTime = System.currentTimeMillis();
         Game completeGame = new GameProcessor().solveGame(startingNumber, targetNumber);
+        logger.trace("getFizzBuzzCompletedGame request completed in " + (System.currentTimeMillis() - startTime) + "ms");
         return completeGame;
     }
 }
--- src/main/java/com/minigames/fizzbuzz/functional/GameProcessor.java
+++ src/main/java/com/minigames/fizzbuzz/functional/GameProcessor.java
@@ -1,10 +1,15 @@
 package com.minigames.fizzbuzz.functional;
 
 import com.minigames.fizzbuzz.representation.Game;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
 public class GameProcessor {
+
+    Logger logger = LoggerFactory.getLogger(GameProcessor.class);
+
     public Game solveGame (int startingNumber, int targetNumber) {
         meetingRequirements(startingNumber, targetNumber);
         Game game = new Game();
@@ -20,13 +25,16 @@ public class GameProcessor {
     }
 
     private void meetingRequirements(int startingNumber, int targetNumber) {
+        //FizzBuzz rules say 1 is the minimum number to start with
         if (startingNumber < 1) {
             //an exception will be thrown
             System.err.println("Invalid starting number: too small");
         }
+        //It also wouldn't make sense if the starting number is greater than the targetNumber
         if (startingNumber > targetNumber) {
             //an exception will be thrown
             System.err.println("Invalid starting number: greater than the maximum allowed (" + targetNumber + ")");
         }
+        logger.info("Game input parameters (" + startingNumber + ", " + targetNumber + ") are correct, ready to begin");
     }
 }
